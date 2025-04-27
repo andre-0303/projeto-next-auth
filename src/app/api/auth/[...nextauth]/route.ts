90859a31275cfa5e138871ac91701e23808da4bb
andre-0303 <bandeiradev.ti@gmail.com>
2025-04-27 19:02:03 -0300

    feat:implementando o supabase

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,46 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import { signIn } from "next-auth/react"
+import { supabase } from "@/lib/supabase"; 
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
-    pages: {
-        signIn: '/',
-    },
   providers: [
     CredentialsProvider({
       name: 'Credentials',
       credentials: {
-        email: { label: "Email", type: "email"},
+        email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" }
       },
       async authorize(credentials, req) {
-        if(!credentials) {
-            return null;
+        if (!credentials) {
+          return null;
         }
-    
-        if (credentials.email === "zidas123@gmail.com" && credentials.password === '123456') {
-            return {
-                id: '1',
-                name: 'Zidas',
-                email: 'zidas123@gmail.com'
-            }
-        }
-    
-        return null; // <---- IMPORTANTE!
-    }    
-    })
-  ]
-})
 
-export { handler as GET, handler as POST }
+        const { email, password } = credentials;
+
+        const { data: user, error } = await supabase
+          .from('users')
+          .select('*')
+          .eq('email', email)
+          .single();
+
+        if (error || !user) {
+          console.log('Usuário não encontrado');
+          return null;
+        }
+
+        if (user.password !== password) {
+          console.log('Senha inválida');
+          return null;
+        }
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        };
+      }
+    })
+  ],
+  pages: {
+    signIn: '/'
+  }
+});
